ad660f3f35c4f7da42adf5a4041a918460b168f7
Соколов Ярослав <yari.sokolov.2004@gmail.com>
2024-09-29 15:34:37 +0300

    Динамический URL. Пользовательский конвертер

--- core/women/urls.py
+++ core/women/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, register_converter
 
-from women import views
+from women import converters, views
+
+register_converter(converters.FourDigitYearConverter, type_name="year4")
 
 urlpatterns = [
     path("", views.index),  # http://127.0.0.1:8000
-    path("cats/", views.categories),  # http://127.0.0.1:8000/cats/
+    path("cats/<int:cat_id>/", views.categories),  # http://127.0.0.1:8000/cats/
+    path("archive/<year4:year>/", views.archive),
 ]
--- core/women/views.py
+++ core/women/views.py
@@ -5,5 +5,9 @@ def index(request) -> HttpResponse:
     return HttpResponse("Стартовая страничка")
 
 
-def categories(request) -> HttpResponse:
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request, cat_id) -> HttpResponse:
+    return HttpResponse(f"<h1>Статьи по категориям id={cat_id}</h1>")
+
+
+def archive(request, year: int) -> HttpResponse:
+    return HttpResponse(f"<h1>Архив по годам Год: {year}</h1>")
